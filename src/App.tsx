0f26c252634aa23e329d6810267f20aca59ed381
DogeQuest <dogequest740@gmail.com>
2026-02-12 02:03:47 +0300

    Update landing contract address

--- src/App.tsx
+++ src/App.tsx
@@ -741,7 +741,7 @@ const STARTER_PACK_ITEMS: { type: ConsumableType; qty: number }[] = [
   { type: 'attack', qty: 10 },
   { type: 'key', qty: 20 },
 ]
-const CONTRACT_ADDRESS = 'Soon'
+const CONTRACT_ADDRESS = '4tdjcug83aGMgtNyoDErfycjGbzZfAX2jMQgcAKMpump'
 
 const MONSTER_HP_TIER_TARGET = 30000
 const MONSTER_HP_TIER_EXCESS = 0.2
