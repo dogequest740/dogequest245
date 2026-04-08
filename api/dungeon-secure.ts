17ba70127eeda4d43b0692ef3b5c42978646410c
vaska <dogequest740@gmail.com>
2026-04-08 18:58:53 +0300

    Revert "Proxy secure auth calls through Vercel"
    This reverts commit d9708e50d1fec210379388cb028c5136428c7113.

--- api/dungeon-secure.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import forwardSecureRequest from './_secureProxy'
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  return forwardSecureRequest(req, res, 'dungeon-secure')
-}
